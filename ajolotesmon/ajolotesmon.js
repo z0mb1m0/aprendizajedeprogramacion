3586ce6a8fe1884d59e5fb8d3159b03d4d6cb183
mr.zeta <zombi64chavezcuevas@gmail.com>
2022-09-16 14:19:44 -0500

    creando el inicio de ajolotesmon

---
+++ ajolotesmon/ajolotesmon.js
@@ -0,0 +1,6 @@
+function seleccionarAjoloteJugador() {
+    alert("SELECCIONASTE TU AJOLOTE")
+}
+
+let botonAjoloteJugador = document.getElementById('boton-ajolote')
+botonAjoloteJugador.addEventListener('click', seleccionarAjoloteJugador)
